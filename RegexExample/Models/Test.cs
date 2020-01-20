1ae5b8112e9d30620414fd83986111e83ce62db0
William Francois <william.francois@outlook.com>
2020-01-20 19:36:36 +0100

    WFR

---
+++ RegexExample/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegexExample.Models
+{
+	public class Test
+	{
+		public int TestID { get; set; }
+		[RegularExpression(@"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)")]
+		public string ValueExponent { get; set; }
+	}
+}
